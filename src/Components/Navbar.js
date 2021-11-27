835f8b098a8607d3f4457493f51d4e86ecc9a0e0
John Mazuch <36649122+jmazuch001@users.noreply.github.com>
2021-11-26 23:55:11 -0500

    login page ready status

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,15 +5,15 @@ import styles from './Navbar.module.css'
 
 export default function Navbar() {
     return (
-        
-        <nav className={styles.Navbar}>
+        <div>
+        <nav className={styles.navbar}>
             <ul>
                 <li className={styles.title}>Financial Reporting</li>   
                 <li><Link to="/login">Login</Link></li>
-                <li><Link to="/login">Signup</Link></li>   
+                <li><Link to="/signup">Signup</Link></li>   
             </ul>
         </nav>
-        
+        </div>
     ) 
 }
 
