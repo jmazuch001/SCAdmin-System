b5fe5da6385637aafeb4588cafc22084534e2bb2
John Mazuch <36649122+jmazuch001@users.noreply.github.com>
2021-12-06 15:46:32 -0500

    fixed scaling of background image for pages

---
+++ src/Components/Workflows.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Offcanvas } from 'react-bootstrap';
+import {Link} from 'react-router-dom'
+// styles
+import styles from './Navbar.module.css'
+
+export default function Workflows() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+    return (
+        
+
+        <div>
+            <Navbar>
+                <Button>
+                    Launch Workflows
+                </Button>
+            </Navbar>
+        
+            <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>
+                        Welcome to Workflows!
+                        <p>Please select a Workflow to begin.</p>
+                    </Offcanvas.Title>
+                </Offcanvas.Header>
+            </Offcanvas>
+            
+            
+        
+        </div>
+    ) 
+}
