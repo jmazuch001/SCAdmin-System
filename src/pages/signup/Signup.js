b7bcbf1980148839debab932ad6fe606049fb80f
John Mazuch <36649122+jmazuch001@users.noreply.github.com>
2021-12-06 21:00:45 -0500

    setting up firebase db

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,11 +1,47 @@
 import styles from './signup.module.css'
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+
 export default function Signup() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [displayName, setDisplayName] = useState('')
+
+    const handleSubmit = (e) => {
+        // prevent default action of page reload
+        e.preventDefault()
+        console.log(email, password, displayName)
+    }
     return (
-        <div>
-            Signup
+        <div className={styles['signup-body']}>
+            
+        <form onSubmit={handleSubmit} className={styles['signup-form']}>
+            <h2 className={styles.signup}>Signup</h2>
+            <label className={styles.label}>
+                <span>email:</span>
+                <input
+                    type="email"
+                    onChange = {(e) => setEmail(e.target.value)}
+                    value={email}   
+                />
+            </label>
+            <label>
+                <span>Password:</span>
+                <input type="password" onChange = {(e) => setPassword(e.target.value)}
+                value = {password}
+                />
+                
+            </label>
+            <label>
+                <span>Display Name:</span>
+                <input type="password" onChange = {(e) => setDisplayName(e.target.value)}
+                value = {displayName}
+                />
+                
+            </label>
+            <button className="btn button">signup</button>
+        </form>
         </div>
     )
 }
