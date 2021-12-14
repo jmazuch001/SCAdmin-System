e806f846d4d8c3486ba6ecab9f1ab3cf4b68f1df
John Mazuch <36649122+jmazuch001@users.noreply.github.com>
2021-12-14 16:26:31 -0500

    adding cleanup functions

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+
 // styles
 import styles from './Navbar.module.css'
 
 export default function Navbar() {
+    const { logout } = useLogout()
+
     return (
         <div>
         <nav className={styles.navbar}>
@@ -15,6 +19,7 @@ export default function Navbar() {
                 <li><Link to="/signup" className={styles['link-text']}>Signup</Link></li>   
                 <li><Link to="/AccessControl" className={styles['link-text']}>QR Code Generator</Link></li>
                 <li><Link to="/office" className={styles['link-text']}>Office</Link></li>
+                <li><button className="logout-btn" onClick={logout}>Logout</button></li>
             </ul>
             
         </nav>
