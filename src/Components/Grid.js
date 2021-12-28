2b8507726643e49827671dacfc61b9fa516cd01b
John Mazuch <36649122+jmazuch001@users.noreply.github.com>
2021-12-28 02:12:07 -0500

    progress made on image overlay in GridLayout

---
+++ src/Components/Grid.js
@@ -0,0 +1,55 @@
+import styles from './home.module.css'
+import React, { Component } from 'react'
+import { Jumbotron, Button } from 'react-bootstrap'
+import TransactionForm from './TransactionForm'
+import StepSystem from './StepSystem'
+import Workflows from '../../Components/Workflows'
+import AdminWorkflows from '../office/AdminWorkflows'
+import { Divider, Grid, Image, Segment, Item } from "semantic-ui-react";
+import { Tab } from "semantic-ui-react";
+import { Container } from 'semantic-ui-react'
+
+export default function() {
+    return (
+        <>
+       <Image src='https://cdn.mos.cms.futurecdn.net/QptEvvA25MM4MgbqVuFf5i.jpg' />
+ 
+    <Segment >
+      <Grid columns={2} relaxed="very" >
+        <Grid.Column >
+            <Item>
+            <h1>Exploration</h1>
+          <p>
+            <Image src="https://twinfinite.net/wp-content/uploads/2021/12/Star-Citizen.jpg" />
+          </p>
+            </Item>
+          
+        </Grid.Column>
+        <Grid.Column>
+          <h1>Dogfighting</h1>
+          <p>
+            <Image src="https://cdn.mos.cms.futurecdn.net/QptEvvA25MM4MgbqVuFf5i.jpg" />
+          </p>
+        </Grid.Column>
+      </Grid>
+
+      <Divider vertical className={styles.gridLayout}>And</Divider>
+      <Grid columns={2} relaxed="very">
+        <Grid.Column>
+          <h1>Salvage</h1>
+          <p>
+            <Image src="https://imgix.kotaku.com.au/content/uploads/sites/3/2019/12/star-citizen-4k-1.jpg?ar=16%3A9&auto=format&fit=crop&q=65&w=1280" />
+          </p>
+        </Grid.Column>
+        <Grid.Column>
+          <h1>Mining</h1>
+          <p>
+            <Image src="https://www.xfire.com/wp-content/uploads/2021/03/starcitizen.jpg" />
+          </p>
+        </Grid.Column>
+      </Grid>
+    </Segment>
+    </>
+    )
+}
+
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,28 +5,59 @@ import TransactionForm from './TransactionForm'
 import StepSystem from './StepSystem'
 import Workflows from '../../Components/Workflows'
 import AdminWorkflows from '../office/AdminWorkflows'
+import { Divider, Grid, Image, Segment, Item } from "semantic-ui-react";
+import { Tab } from "semantic-ui-react";
+import { Container } from 'semantic-ui-react'
 
+const panes = [
+    { menuItem: "Tab 1", render: () => <Tab.Pane>Step 1<StepSystem /></Tab.Pane> },
+    { menuItem: "Tab 2", render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+    { menuItem: "Tab 3", render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> }
+  ];
 
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+    
+        <>
+        <Image src='https://images.pexels.com/photos/2538107/pexels-photo-2538107.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' />
+        <section>
+            <div className={styles.container}>
+            <Tab panes={panes} defaultActiveIndex={0} className={styles.tabNav} />
+           
+           
+           
             
-            <div>
-                <StepSystem />
+           <div>
+           <h1 className="orgSpan">What Our Organization Does:</h1>
+           <p>
+               We bring other organizations together in a common cause to provide 
+               an environment where all can thrive and successfully operate under the 
+               same set of agreed-upon rules. 
+           </p>
+           <h3>
+               Why trust our corporate umbrella?
+           </h3>
+           <p>
+               Never stifled by over-regulation, all members
+               are encouraged to bring unique solutions to the table.
+           </p>
             </div>
-            <div className={styles.content}>
-                Transaction List
-            </div>
-            <div className={styles.sidebar}>
-                <TransactionForm />
-            </div> 
-            <div>
-                <AdminWorkflows />
-            </div> 
+           
+                
+    
+    
+    
+  </div>
+  </section>
+        </>
+       
+             
+            
 
             
-        </div>
+        
+        
     )
 }
 
