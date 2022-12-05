580218a786fce0d6b50ce0d2dae72010aa792f63
John Mazuch <36649122+jmazuch001@users.noreply.github.com>
2022-12-04 21:39:01 -0500

    rework of top navbar component to mui specs

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,6 +8,8 @@ import { useAuthContext } from '../hooks/useAuthContext'
 // styles
 import styles from './Navbar.module.css'
 import { AppBar, Drawer, Toolbar, Typography } from '@mui/material'
+import { List, ListItem } from 'semantic-ui-react'
+import Home from '../pages/home/Home'
 
 export default function Navbar() {
     const { logout, isPending } = useLogout()
@@ -33,7 +35,7 @@ export default function Navbar() {
                 {user && (
                     <>
                         <li>Hello, {user.displayName}</li>
-                        <li><Link to="/AccessControl" className={styles['link-text']}>QR Code Generator</Link></li>
+                        {/* <li><Link to="/AccessControl" className={styles['link-text']}>QR Code Generator</Link></li> */}
                         <li><Link to="/office" className={styles['link-text']}>Office</Link></li>
                         <li><Link to="/" className={styles['link-text']}>Home</Link></li>
                         <li>
@@ -45,21 +47,31 @@ export default function Navbar() {
             </ul>
             
         </nav>
-        <div>
+        {/* <div>
             <AppBar position="fixed">
                 <Toolbar>
-                    <Typography>
-                        Advanced material UI Styling is here
+                    <Typography variant="h6" noWrap>
+                        Advanced Material UI Styling is here
                     </Typography>
+                    
                 </Toolbar>   
             </AppBar>
-            <Drawer>
-
+            <Drawer
+                variant='temporary'
+                open={true}
+            >
+                <List>
+                    {[{ text: "Home", route: "/"}, { text: "Contact Card Grid"}].map((nav, items) => (
+                        <ListItem key={nav.text}><Link to={nav.route}>{nav.text}</Link></ListItem>
+                    ))}
+                </List>
             </Drawer>
             <main>
-
+                <Routes>
+                    <Route path={"/"} element={<Home />}/>
+                </Routes>
             </main>
-        </div>
+        </div> */}
         </div>
         
     ) 
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,47 +1,67 @@
 import React, { useState, useEffect } from 'react'
 import { withRouter } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import CreateProject from '../pages/create/CreateProject';
 import OnlineUsers from './OnlineUsers';
 import styles from './Sidebar.module.css'
 import Container from '@mui/material/Container'
 import { useAuthContext } from '../hooks/useAuthContext'
+import { AppBar, Drawer, Typography, List, ListItem } from '@mui/material';
 
 export default function SideNav () {
   const { user } = useAuthContext()
   return (
-    <div className={styles.sidebar}>
-      <div className={styles['sidebar-content']}>
-        <div className={styles.user}>
-          {/* avatar & username here later */}
-          <p>Hey {user.displayName}</p>  
-        </div>  
-        <nav className={styles.links}>
-          <ul>
-            <li>
-              <NavLink exact to="/">
-                <span>Home</span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/CreateProject">
-                <span>New Project</span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to='/Dashboard'>
-                <span>Dashboard</span>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to='/Reporting'>
-                <span>Reporting</span>
-              </NavLink>
-            </li>
-          </ul>
+    // <div className={styles.sidebar}>
+    //   <div className={styles['sidebar-content']}>
+    //     <div className={styles.user}>
+    //       <p>Hey {user.displayName}</p>  
+    //     </div>  
+    //     <nav className={styles.links}>
+    //       <ul>
+    //         <li>
+    //           <NavLink exact to="/">
+    //             <span>Home</span>
+    //           </NavLink>
+    //         </li>
+    //         <li>
+    //           <NavLink to="/CreateProject">
+    //             <span>New Project</span>
+    //           </NavLink>
+    //         </li>
+    //         <li>
+    //           <NavLink to='/Dashboard'>
+    //             <span>Dashboard</span>
+    //           </NavLink>
+    //         </li>
+    //         <li>
+    //           <NavLink to='/Reporting'>
+    //             <span>Reporting</span>
+    //           </NavLink>
+    //         </li>
+    //       </ul>
           
-        </nav>
-      </div>
+    //     </nav>
+    //   </div>
+    // </div>
+    <div>
+      <Drawer 
+      variant="temporary"
+      open={true}
+      >
+        <Typography variant="h6" noWrap>
+          Hey {user.displayName}
+        </Typography>
+        <List>
+          {[
+            { text: "Home", route:"/"}, 
+            { text: "Create Project", route:"/CreateProject"}, 
+            { text: "Dashboard", route:"/Dashboard"}, 
+            { text: "Reporting", route:"/Reporting"}
+          ].map((sidenav, list) => (
+            <ListItem key={sidenav.text}><Link to={sidenav.route}>{sidenav.text}</Link></ListItem>
+          ))}
+        </List>
+      </Drawer>
     </div>
   )
 }
